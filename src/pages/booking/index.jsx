32aaddb43fa05d43d38ec2cf219f8eddc577da26
Alam Rasyidi <alam.dp99@gmail.com>
2019-01-14 16:25:34 +0700

    booking

--- src/pages/booking/index.jsx
+++ src/pages/booking/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Form, Input, Select, DatePicker, Button, Card, Row, Col } from "antd";
 import "antd/dist/antd.css";
+import "./booking.css";
 
 const areaData = [
   "GedungA",
@@ -42,9 +43,21 @@ export default class BookingForm extends Component {
   };
 
   render() {
-    const formItemLayout = {
+    const areaFormItemLayout = {
       labelCol: { span: 4, offset: 0 },
-      wrapperCol: { span: 18, offset: 2 }
+      wrapperCol: { span: 6, offset: 2 }
+    };
+    const ruangFormItemLayout = {
+      labelCol: { span: 4, offset: 0 },
+      wrapperCol: { span: 3, offset: 2 }
+    };
+    const eventFormItemLayout = {
+      labelCol: { span: 4, offset: 0 },
+      wrapperCol: { span: 16, offset: 2 }
+    };
+    const tanggalFormItemLayout = {
+      labelCol: { span: 4, offset: 0 },
+      wrapperCol: { span: 16, offset: 2 }
     };
     const { areaRooms } = this.state;
 
@@ -54,61 +67,76 @@ export default class BookingForm extends Component {
           type="flex"
           justify="center"
           align="middle"
-          style={{ padding: 130 }}
+          style={{ padding: 108 }}
         >
           <Col span={100}>
-            <Card style={{ width: 900, padding: 0 }}>
-              <Card.Grid style={{padding: 0, height: "100%", width: "40%" }}>
-                <Card style={{padding: 10}} cover={<img alt="Failed to load" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQym9v4l9S1Ma9vhmj5QCq3-88YuprHRx7E2gN0ByBOD_zZqLR3" />}>
-                  <Button style={{margin: 15}} type="primary">
+            <Card style={{ width: 900, padding: 0 }} id="booking">
+              <Card.Grid style={{ padding: 0, height: "100%", width: "40%" }}>
+                <Card
+                  style={{ padding: 10 }}
+                  cover={
+                    <img
+                      alt="Failed to load"
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQym9v4l9S1Ma9vhmj5QCq3-88YuprHRx7E2gN0ByBOD_zZqLR3"
+                    />
+                  }
+                >
+                  <Button
+                    className="picture-desc-button"
+                    style={{ margin: 30 }}
+                    type="primary"
+                  >
                     Deskripsi
                   </Button>
                 </Card>
               </Card.Grid>
+
               <Card.Grid style={{ height: "100%", width: "60%" }}>
-                  <Form layout="horizontal">
-                    <Form.Item label="Event" {...formItemLayout} required>
-                      <Input />
-                    </Form.Item>
+                <Form layout="horizontal">
+                  <Form.Item label="Event" {...eventFormItemLayout} required>
+                    <Input />
+                  </Form.Item>
 
-                    <Form.Item label="Area" {...formItemLayout} required>
-                      <Select
-                        defaultValue="Gedung A"
-                        onChange={this.handleRuangChange}
-                      >
-                        {areaData.map(area => (
-                          <Select.Option key={area}>
-                            {area.replace("ung", "ung ")}
-                          </Select.Option>
-                        ))}
-                      </Select>
-                    </Form.Item>
+                  <Form.Item label="Area" {...areaFormItemLayout} required>
+                    <Select
+                      defaultValue="Gedung A"
+                      onChange={this.handleRuangChange}
+                    >
+                      {areaData.map(area => (
+                        <Select.Option key={area}>
+                          {area.replace("ung", "ung ")}
+                        </Select.Option>
+                      ))}
+                    </Select>
+                  </Form.Item>
 
-                    <Form.Item label="Ruang" {...formItemLayout} required>
-                      <Select
-                        onChange={this.onRoomChange}
-                        value={this.state.room}
-                      >
-                        {areaRooms.map(ruang => (
-                          <Select.Option key={ruang}>{ruang}</Select.Option>
-                        ))}
-                      </Select>
-                    </Form.Item>
+                  <Form.Item label="Ruang" {...ruangFormItemLayout} required>
+                    <Select
+                      onChange={this.onRoomChange}
+                      value={this.state.room}
+                    >
+                      {areaRooms.map(ruang => (
+                        <Select.Option key={ruang}>{ruang}</Select.Option>
+                      ))}
+                    </Select>
+                  </Form.Item>
 
-                    <Form.Item label="Tanggal" {...formItemLayout} required>
-                      <DatePicker.RangePicker
-                        format="DD-MM-YYYY HH:mm"
-                        placeHolder={["Mulai", "Selesai"]}
-                        showTime={{ format: "HH:mm" }}
-                      />
-                    </Form.Item>
+                  <Form.Item
+                    label="Tanggal"
+                    {...tanggalFormItemLayout}
+                    required
+                  >
+                    <DatePicker.RangePicker
+                      format="DD-MM-YYYY h:mm a"
+                      placeHolder={["Mulai", "Selesai"]}
+                      showTime={{ format: "h:mm a" }}
+                    />
+                  </Form.Item>
 
-                    <Form.Item>
-                      <Button type="primary" htmType="submit">
-                        Submit
-                      </Button>
-                    </Form.Item>
-                  </Form>
+                  <Form.Item style={{ margin: "auto" }}>
+                    <Button type="primary">Submit</Button>
+                  </Form.Item>
+                </Form>
               </Card.Grid>
             </Card>
           </Col>
