b633949651a02065b5e2c55ceb102c9f62a15f83
rezak400 <k400ezza@gmail.com>
2019-01-15 17:58:56 +0700

    a

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,11 +4,11 @@ import "./footer.css"
 export default function Footer() {
   return (
     <div>
-<footer>
-  <div class="container">
-    <p>AsaBarbar<a href=""> Copyright</a> SMKN 1</p>
-  </div>
-</footer>
+      <footer>
+        <div class="container">
+          <p>AsaBarbar<a href=""> Copyright</a> SMKN 1</p>
+        </div>
+      </footer>
     </div>
   )
 }
