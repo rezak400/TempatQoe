03f36a97c3d938537e746859c32423e4000a4039
Alam Rasyidi <alam.dp99@gmail.com>
2019-01-16 16:11:42 +0700

    commit

--- src/pages/booking/index.jsx
+++ src/pages/booking/index.jsx
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
-import { Form, Input, Select, DatePicker, Button, Card, Row, Col } from "antd";
+import {
+  Form,
+  Input,
+  Select,
+  DatePicker,
+  Button,
+  Card,
+  Row,
+  Col,
+  InputNumber
+} from "antd";
 import "antd/dist/antd.css";
 import "./booking.css";
 
@@ -12,10 +22,10 @@ const areaData = [
   "RPS"
 ];
 const ruangData = {
-  GedungA: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  GedungB: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  GedungC: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  GedungD: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  GedungA: ["A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9"],
+  GedungB: ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9"],
+  GedungC: ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9"],
+  GedungD: ["D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9"],
   Lapangan: [" "],
   RPS: [" "]
 };
@@ -43,17 +53,17 @@ export default class BookingForm extends Component {
   };
 
   render() {
-    const areaFormItemLayout = {
-      labelCol: { span: 4, offset: 0 },
-      wrapperCol: { span: 6, offset: 2 }
-    };
-    const ruangFormItemLayout = {
-      labelCol: { span: 4, offset: 0 },
-      wrapperCol: { span: 3, offset: 2 }
-    };
     const eventFormItemLayout = {
       labelCol: { span: 4, offset: 0 },
       wrapperCol: { span: 16, offset: 2 }
+    };
+    const jumlahRuangFormItemLayout = {
+      labelCol: { span: 4, offset: 0 },
+      wrapperCol: { offset: 6 }
+    };
+    const ruangFormItemLayout = {
+      labelCol: { span: 4, offset: 0 },
+      wrapperCol: { offset: 6 }
     };
     const tanggalFormItemLayout = {
       labelCol: { span: 4, offset: 0 },
@@ -70,7 +80,11 @@ export default class BookingForm extends Component {
           style={{ padding: 108 }}
         >
           <Col span={100}>
-            <Card style={{ width: 900, padding: 0 }} id="booking">
+            <Card
+              style={{ width: 1000, padding: 0 }}
+              id="booking"
+              bordered={false}
+            >
               <Card.Grid style={{ padding: 0, height: "100%", width: "40%" }}>
                 <Card
                   style={{ padding: 10 }}
@@ -97,28 +111,35 @@ export default class BookingForm extends Component {
                     <Input />
                   </Form.Item>
 
-                  <Form.Item label="Area" {...areaFormItemLayout} required>
-                    <Select
-                      defaultValue="Gedung A"
-                      onChange={this.handleRuangChange}
-                    >
-                      {areaData.map(area => (
-                        <Select.Option key={area}>
-                          {area.replace("ung", "ung ")}
-                        </Select.Option>
-                      ))}
-                    </Select>
+                  <Form.Item
+                    label="Jumlah Ruang"
+                    {...jumlahRuangFormItemLayout}
+                  >
+                    <InputNumber defaultValue={1} />
                   </Form.Item>
 
                   <Form.Item label="Ruang" {...ruangFormItemLayout} required>
-                    <Select
-                      onChange={this.onRoomChange}
-                      value={this.state.room}
-                    >
-                      {areaRooms.map(ruang => (
-                        <Select.Option key={ruang}>{ruang}</Select.Option>
-                      ))}
-                    </Select>
+                    <Input.Group compact={true}>
+                      <Select
+                        defaultValue="Gedung A"
+                        onChange={this.handleRuangChange}
+                      >
+                        {areaData.map(area => (
+                          <Select.Option key={area}>
+                            {area.replace("ung", "ung ")}
+                          </Select.Option>
+                        ))}
+                      </Select>
+
+                      <Select
+                        onChange={this.onRoomChange}
+                        value={this.state.room}
+                      >
+                        {areaRooms.map(ruang => (
+                          <Select.Option key={ruang}>{ruang}</Select.Option>
+                        ))}
+                      </Select>
+                    </Input.Group>
                   </Form.Item>
 
                   <Form.Item
