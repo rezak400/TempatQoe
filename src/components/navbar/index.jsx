8cc468b08a3ea0d11d6ac71d4b6971b2534bafa0
rezak400 <k400ezza@gmail.com>
2019-01-21 16:12:04 +0700

    navbar mantap

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { Component } from "react";
 import {Link} from 'react-router-dom'
 import "./navbar.css"
 
-export default function Navbar() {
+export default class Navbar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+        openDrawer : false
+    };
+  };
+      
+  // changeVar = ()=>{
+  //     this.setState({ openDrawer: !this.state.openDrawer})
+  //     console.log(this.state.openDrawer)
+  // }
+  
+  render(){
+
+  let changeVar = () => {
+    this.setState({ openDrawer : !this.state.openDrawer})
+  }
+   
   return (
     <div className="nav">
-    
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><a href="#contact" to="/">Contact</a></li>
-        <li style={{ float: "right"}}><a class="active" href="#about">About</a></li>
-      </ul>
+        
+        <div className={this.state.openDrawer ? 'topnav responsive' : 'topnav'} id="myTopnav">
+          <Link to="/">Home</Link>
+          <Link to="/booking">Booking</Link>
+          <Link className="login-kanan" to="/login">Login</Link>
+          
+          {/* <div class="dropdown">
+            <button class="dropbtn">Dropdown 
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+              <a href="#">Link 1</a>
+              <a href="#">Link 2</a>
+              <a href="#">Link 3</a>
+            </div>
+          </div> */}
+          
+          
+          <a style={{fontSize:"17px"}} class="icon" onClick={changeVar}>&#9776;</a>
+        </div>
+
+       
     
     </div>
   )
+}
 }
