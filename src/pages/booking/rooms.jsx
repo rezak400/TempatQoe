ae203dcc27b7074ea0f8988674b822d8077178de
Alam Rasyidi <alam.dp99@gmail.com>
2019-01-22 13:39:35 +0700

    cascader update

---
+++ src/pages/booking/rooms.jsx
@@ -0,0 +1,216 @@
+export default Option = [
+    {
+      value: "Gedung A",
+      label: "Gedung A",
+      children: [
+        {
+          value: "A1",
+          label: "A1",
+          disabled: false
+        },
+        {
+          value: "A2",
+          label: "A2",
+          disabled: false
+        },
+        {
+          value: "A3",
+          label: "A3",
+          disabled: false
+        },
+        {
+          value: "A4",
+          label: "A4",
+          disabled: false
+        },
+        {
+          value: "A5",
+          label: "A5",
+          disabled: false
+        },
+        {
+          value: "A6",
+          label: "A6",
+          disabled: false
+        },
+        {
+          value: "A7",
+          label: "A7",
+          disabled: false
+        },
+        {
+          value: "A8",
+          label: "A8",
+          disabled: false
+        },
+        {
+          value: "A9",
+          label: "A9",
+          disabled: false
+        }
+      ]
+    },
+    {
+      value: "Gedung B",
+      label: "Gedung B",
+      children: [
+        {
+          value: "B1",
+          label: "B1",
+          disabled: false
+        },
+        {
+          value: "B2",
+          label: "B2",
+          disabled: false
+        },
+        {
+          value: "B3",
+          label: "B3",
+          disabled: false
+        },
+        {
+          value: "B4",
+          label: "B4",
+          disabled: false
+        },
+        {
+          value: "B5",
+          label: "B5",
+          disabled: false
+        },
+        {
+          value: "B6",
+          label: "B6",
+          disabled: false
+        },
+        {
+          value: "B7",
+          label: "B7",
+          disabled: false
+        },
+        {
+          value: "B8",
+          label: "B8",
+          disabled: false
+        },
+        {
+          value: "B9",
+          label: "B9",
+          disabled: false
+        }
+      ]
+    },
+    {
+      value: "Gedung C",
+      label: "Gedung C",
+      children: [
+        {
+          value: "C1",
+          label: "C1",
+          disabled: false
+        },
+        {
+          value: "C2",
+          label: "C2",
+          disabled: false
+        },
+        {
+          value: "C3",
+          label: "C3",
+          disabled: false
+        },
+        {
+          value: "C4",
+          label: "C4",
+          disabled: false
+        },
+        {
+          value: "C5",
+          label: "C5",
+          disabled: false
+        },
+        {
+          value: "C6",
+          label: "C6",
+          disabled: false
+        },
+        {
+          value: "C7",
+          label: "C7",
+          disabled: false
+        },
+        {
+          value: "C8",
+          label: "C8",
+          disabled: false
+        },
+        {
+          value: "C9",
+          label: "C9",
+          disabled: false
+        }
+      ]
+    },
+    {
+      value: "Gedung D",
+      label: "Gedung D",
+      children: [
+        {
+          value: "D1",
+          label: "D1",
+          disabled: false
+        },
+        {
+          value: "D2",
+          label: "D2",
+          disabled: false
+        },
+        {
+          value: "D3",
+          label: "D3",
+          disabled: false
+        },
+        {
+          value: "D4",
+          label: "D4",
+          disabled: false
+        },
+        {
+          value: "D5",
+          label: "D5",
+          disabled: false
+        },
+        {
+          value: "D6",
+          label: "D6",
+          disabled: false
+        },
+        {
+          value: "D7",
+          label: "D7",
+          disabled: false
+        },
+        {
+          value: "D8",
+          label: "D8",
+          disabled: false
+        },
+        {
+          value: "D9",
+          label: "D9",
+          disabled: false
+        }
+      ]
+    },
+    {
+      value: "Lapangan",
+      label: "Lapangan",
+          disabled: false
+    },
+    {
+      value: "RPS",
+      label: "RPS",
+          disabled: false
+    }
+  ]
