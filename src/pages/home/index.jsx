6d6060d184c91aac4f6788bd2a5a42b27e0e3eb8
rezak400 <k400ezza@gmail.com>
2019-01-29 15:31:39 +0700

    tes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -18,6 +18,7 @@ export default class Home extends React.Component{
     <div className="home">
         <div className="atas">
           <Header />  
+          aku
           <Button />
             <br/>
             <br/> 
